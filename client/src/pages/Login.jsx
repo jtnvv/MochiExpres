16cb68899e7ecbcaf041e1e165269f05cc7fa257
Jonathan <jonathan.jnthnd@gmail.com>
2023-10-03 13:06:57 -0500

    Error message and changes in gui :3

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import people from "../img/login-people.png";
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    function verificacionCampos(event) //verifica si ambos campos estan llenos
+    {
+        event.preventDefault();
+        if (username === "" || password === "") {
+            setErrorMessage("Por favor, llena ambos campos");
+        }
+        else {
+            //continuar con el proceso de login
+        }
+    }
     return (
         <div className="auth">
             <div className="auth__bg">
                 <h1>¡Bienvenido!</h1>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <span><Link to="/Recuperacion-Contrasena">¿Olvidaste tu contraseña?</Link></span>
+                <form onSubmit={verificacionCampos}>
+                    <input type="text" id="username" name="username" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)} />
+                    <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                    {errorMessage && <p className="auth__bg__error">{errorMessage}</p>}
+                    <Link className="auth__bg__text" to="/Recuperacion-Contrasena">¿Olvidaste tu contraseña?</Link>
                     <button type="submit">Entra!</button>
-                    <span><Link to="/Registro">¿No tienes cuenta? Unete</Link></span>
+                    <Link className="auth__bg__text" to="/Registro">¿No tienes cuenta? Unete</Link>
                 </form>
             </div>
             <div className="auth__img">
-                <img src={people} alt="imagen diseño" className="auth_img_img"/>
+                <img src={people} alt="imagen diseño" className="auth_img_img" />
             </div>
         </div>
     );
