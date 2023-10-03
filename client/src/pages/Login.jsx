42d1575bbf21dfb335ee9abcedc1df3d2fbf0736
Jonathan <jonathan.jnthnd@gmail.com>
2023-10-03 00:01:20 -0500

    la vida de un critico es sencilla

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div className="auth">
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+export default Login;
---
+++ client/src/pages/PersonalInfo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PersonalInfo = () => {
+    return (
+        <div>
+            ola
+        </div>
+    );
+};
+export default PersonalInfo;
