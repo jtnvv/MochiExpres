409303f03bfedadac2a832d0c99778be919b2112
Kelly Solano <kelly.solano.1403@gmail.com>
2023-10-08 22:23:11 -0500

    Funcion de logout

--- client/src/pages/Sidebar.jsx
+++ client/src/pages/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import * as BiIcons from "react-icons/bi";
 import * as MdIcons from "react-icons/md";
@@ -6,54 +6,67 @@ import * as PiIcons from "react-icons/pi";
 import * as IoIcons from "react-icons/io";
 import * as FaIcons from "react-icons/fa6";
 import * as FcIcons from "react-icons/fc";
+import { AuthContext } from "../context/authContext";
 const Sidebar = () => {
-  
-     return (
+
+    const { currentUser, logout } = useContext(AuthContext);
+
+    return (
         <div className="Sidebar">
             <div className="Dashboard">
-                <ul style={{listStyleType:"none"}}>
+                <ul style={{ listStyleType: "none" }}>
                     <li>
                         <Link to="#" className="Inicio">
-                            <FcIcons.FcInTransit className="IconColor"/> 
+                            <FcIcons.FcInTransit className="IconColor" />
                         </Link>
                     </li>
                     <li>
                         <Link to="/Consultar-Pedidos" className="Consultar pedidos">
-                            <BiIcons.BiSolidShoppingBags className="IconColor"/>
+                            <BiIcons.BiSolidShoppingBags className="IconColor" />
                         </Link>
                     </li>
                     <li>
-                        <Link to="/Habilitar-Solicitud" className="Habilitar Solicitudes"> 
-                            <MdIcons.MdNotificationAdd className="IconColor"/>
+                        <Link to="/Habilitar-Solicitud" className="Habilitar Solicitudes">
+                            <MdIcons.MdNotificationAdd className="IconColor" />
                         </Link>
                     </li>
                     <li>
                         <Link to="/Repartidores" className="Repartidores">
-                            <FaIcons.FaTruckArrowRight className="IconColor"/>
+                            <FaIcons.FaTruckArrowRight className="IconColor" />
                         </Link>
                     </li>
                     <li>
                         <Link to="/Clientes" className="Clientes">
-                            <BiIcons.BiSolidHappyBeaming className="IconColor"/>
+                            <BiIcons.BiSolidHappyBeaming className="IconColor" />
                         </Link>
                     </li>
                     <li>
                         <Link to="/Envios" className="Envios">
-                            <PiIcons.PiPackageFill className="IconColor"/>
+                            <PiIcons.PiPackageFill className="IconColor" />
                         </Link>
                     </li>
                 </ul>
                 <div className="Salida">
-                    <ul style={{listStyleType:"none"}}>
-                    <li>
-                        <Link to="/Login" className="Salida">
-                            <IoIcons.IoMdExit className="IconColor"/>
-                        </Link>
-                    </li>
+                    <ul style={{ listStyleType: "none" }}>
+                        <li>
+                            {/* {currentUser ? (
+                                <Link to="#" className="Salida">
+                                    <IoIcons.IoMdExit className="IconColor" onClick={logout} />
+                                </Link>
+                            ) : (
+                                <Link to="\Login" className="Salida">
+                                    <IoIcons.IoMdExit className="IconColor" />
+                                </Link>
+                            )} */}
+
+                            <Link to="/Login" className="Salida">
+                                <IoIcons.IoMdExit className="IconColor" onClick={logout} />
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
-            
+
         </div>
     );
 };
