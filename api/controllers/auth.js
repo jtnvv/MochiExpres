f58d0b1ef9c8bb1caccbd587abfea1272accd48c
Kelly Solano <kelly.solano.1403@gmail.com>
2023-10-11 00:31:20 -0500

    Conexión a bd en la nube :''''D

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
-import { db } from '../db.js';
+import {db}  from '../db.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+// const db = require('../db.js');
 
 export const registerClients = (req, res) => {
 
