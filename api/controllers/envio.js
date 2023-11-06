364103369021f6746cccf0fe33d6bb7d0e3019d2
Kelly Solano <kelly.solano.1403@gmail.com>
2023-11-06 02:19:12 -0500

    correccion de borrado de envio por cliente

--- api/controllers/envio.js
+++ api/controllers/envio.js
@@ -2,29 +2,34 @@ import { db } from '../db.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-export const deleteEnvio = (idCliente) => {
-        
-        let clienteId = idCliente;
+export const deleteEnvio = (req, res) => {
+
+
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("No estas autorizado");
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!");
+        let clienteId = req.params.idCliente;
         console.log("Aqui ", clienteId);
-        if (clienteId === "") {
-            return "No se ha enviado el id del cliente";
-            
-        }
+        if (clienteId === "") return res.status(403).json("No se ha enviado el id del cliente");
 
         const q1 = "SELECT idsolicitudenvio FROM solicitudenvio WHERE idCliente = ?";
-        
+
 
         db.query(q1, [clienteId], (err, solicitudes) => {
-            if (err) return "No se pueden obtener las solicitudes";
-            if (solicitudes.length === 0) return ("No hay solicitudes para este cliente");
+            if (err) return res.status(403).json("No se pueden obtener las solicitudes");
+            if (solicitudes.length === 0) return res.status(404).json("No hay solicitudes para este cliente");
 
             solicitudes.array.forEach(solicitud => {
                 const q2 = "DELETE FROM envio WHERE idsolicitudenvio = ?";
                 db.query(q2, [solicitud.idsolicitudenvio], (err, data) => {
-                    if (err) return ("No se pueden borrar los envios");
+                    if (err) return res.status(403).json("No puedes borrar este envío");
                 });
             });
-            return "Envíos borrados con éxito";
+            return res.status(200).json("Envíos borrados con éxito");
 
         });
+
+    });
+
 }
