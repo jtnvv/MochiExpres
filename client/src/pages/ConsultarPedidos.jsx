87f326e64181f718b827e2ed1f3232a74908d1aa
Kelly Solano <kelly.solano.1403@gmail.com>
2023-11-06 13:03:38 -0500

    avanceeeee en consultar pedidos

--- client/src/pages/ConsultarPedidos.jsx
+++ client/src/pages/ConsultarPedidos.jsx
@@ -1,80 +1,121 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 
 import { Link } from "react-router-dom";
-import Axios from "axios";
 import ModuloClientes from "./ModuloClientes";
-export const selClient = [];
+import { AuthContext } from "../context/authContext";
+export let clienteSeleccionado = [];
 
+//Esto es consultar pedidos realizados por cliente
 const ConsultarPedidos = () => {
-    const [clientsList, setClients] = useState([])
 
-    const getClients =()=>{
-        Axios.get("http://localhost:3005/clients").then((response)=>{
-        setClients(response.data);
-        console.log(typeof(response))
-    })
+    const [clientesList, setClientes] = useState([]);
+    const [enviosList, setEnviosList] = useState([]);
+    const { getClientes } = useContext(AuthContext);
+    const { getEnviosCliente } = useContext(AuthContext);
+
+
+    const [identificador, setIdentificador] = useState({
+        idCliente: ""
+    });
+
+    useEffect(() => {
+        console.log(identificador);
+    }, [identificador]);
+
+    const [err, setError] = useState(null);
+
+    const currentClient = (cliente) => {
+        clienteSeleccionado.pop();
+        clienteSeleccionado.push(cliente);
     }
 
-    const [selectedClient, setSelectedClient] = useState([])
+    useEffect(() => {
+        const obtenerClientes = async () => {
+            try {
+                const res = await getClientes();
+                setClientes(res);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        obtenerClientes();
+    }, [])
 
-    const currentClient =(Client) => {
-           selClient.pop();
-           selClient.push(Client)
 
-    }
 
-    useEffect(()=>{
-        getClients();
-    },[])
-    
-    const [divStyle, setDivStyle] = useState({ });
+    const obtenerEnviosCliente = async () => {
+        try {
+            console.log("Desde aquiii ", identificador.idCliente);
+            const res = await getEnviosCliente(identificador.idCliente);
+            setEnviosList(res);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+
+    useEffect(() => {
+        console.log(enviosList);
+    }, [enviosList]);
+
+    const [divStyle, setDivStyle] = useState({});
     const [clicked, setClicked] = useState(false);
-    const [modalStyle, setModalStyle] = useState({ });
+    const [modalStyle, setModalStyle] = useState({});
     const [clickedModal, setClickedModal] = useState(false);
-    
+
     const handleButtonClick = () => {
-        if (!clicked){
-          setDivStyle({ visibility: 'visible' });
-        }else{
-          setDivStyle({ visibility: 'hidden' });
+        if (!clicked) {
+            setDivStyle({ visibility: 'visible' });
+        } else {
+            setDivStyle({ visibility: 'hidden' });
         }
         setClicked(!clicked);
-      };
-      const showModal = () => {
-          if (!clickedModal){
-              setModalStyle({ visibility: 'visible' });
-          }else{
-              setModalStyle({ visibility: 'hidden' });
-          }
-          setClickedModal(!clickedModal);
-        };
-  
+    };
+    const showModal = (idCliente) => {
+        if (!clickedModal) {
+            setIdentificador({ idCliente: idCliente});
+            setModalStyle({ visibility: 'visible' });
+
+        } else {
+            setModalStyle({ visibility: 'hidden' });
+        }
+        setClickedModal(!clickedModal);
+    };
+
+    useEffect(() => {
+        if(identificador.idCliente !== ""){
+            obtenerEnviosCliente();
+        }
+    },[identificador]);
+
     return (
         <div className="content-flex">
-            <Sidebar/>
+            <Sidebar />
             <div className="divContent">
                 <div className="ItemsContainer">
-                <div className="BarraRepartidor">
+                    <div className="BarraRepartidor">
                         <div className="containerButtonsRepartidor">
                             <h3 className="styleH3Clientes">Consultar pedidos</h3>
-                            </div>
+                        </div>
                         <div className="containerBusquedaRepartidor">
-                            <input type="text" className="BusquedaRepartidor" placeholder="Buscar Cliente"/>
+                            <input type="text" className="BusquedaRepartidor" placeholder="Buscar Cliente" />
                             <div className="InfoBarImg">
-                                <img className="imgPersonalInfo"  src="https://i.imgur.com/T9X0JHm.jpg" alt="" />
+                                <img className="imgPersonalInfo" src="https://i.imgur.com/T9X0JHm.jpg" alt="" />
                             </div>
                         </div>
                     </div>
                     <div className="lista">{
-                        clientsList.map((val,key)=>{
-                            return(
+                        clientesList.map((val) => {
+                            return (
+                                <React.Fragment key={val.idCliente}>
+                                    <div className="ModuloRepartidorContainer" onClick={() => showModal(val.idCliente)}>
+                                        <Link onClick={() => currentClient(val)} style={{ textDecoration: 'none' }}>
+                                            <ModuloClientes nombre={val.nombrecliente} />
+                                        </Link>
+                                    </div>
+                                </React.Fragment>
 
-                                <div className="ModuloRepartidorContainer" onClick={showModal}>                 
-                                    <Link  onClick={() => currentClient(val)} style={{ textDecoration: 'none' }}> 
-                                        <ModuloClientes nombre={val.nombrecliente}  />
-                                    </Link>
-                                </div>
                             );
                         })
                     }
@@ -87,31 +128,31 @@ const ConsultarPedidos = () => {
                     <div className="modalConsultarPedido">
                         <div className="row">
                             <h3>ID</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                         <div className="row">
                             <h3>Fecha envío</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                         <div className="row">
                             <h3>Fecha entrega</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                         <div className="row">
                             <h3>Estado</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                         <div className="row">
                             <h3>Tarifa</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                         <div className="row">
                             <h3>Peso</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                         <div className="row">
                             <h3>Descripción</h3>
-                            <input type="text" disabled className="textareaInput"/>
+                            <input type="text" disabled className="textareaInput" />
                         </div>
                     </div>
                 </div>
