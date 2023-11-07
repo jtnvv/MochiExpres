958d3ade8bc54942578f3a509f0f20609a1ed401
Jonathan <jonathan.jnthnd@gmail.com>
2023-11-07 17:04:22 -0500

    Intento funcionalidad habilitar envio

--- client/src/pages/SolEnvInfo2.jsx
+++ client/src/pages/SolEnvInfo2.jsx
@@ -5,11 +5,46 @@ import { useLocation } from 'react-router-dom';
 import Sidebar from "./Sidebar";
 import { senvioSeleccionado } from "./HabilitarSolicitud";
 import { repartidorSeleccionadoexp } from "./SolEnvInfo1";
-import { getEstado, obtenerValorEstado } from "../components/estadosEnvio.js";
+import { generarIdEnvio } from "../components/generarIdEnvio.js";
 import { Link } from "react-router-dom";
 
 
 const SolEnvInfo2 = () => {
+
+    const [inputs] = useState({
+        idenvio: generarIdEnvio(),
+        descripcionpaquete: senvioSeleccionado[0].descripcionsolicitud,
+        estadoenvio: "En bodega",
+        tarifaenvio: senvioSeleccionado[0].tarifasolicitud,
+        fechaenvioentregado: null,
+        fechaenviorealizado: senvioSeleccionado[0].fechasolicitud,
+        destinoenvio: senvioSeleccionado[0].destinosolicitud,
+        idrepartidor: repartidorSeleccionadoexp[0],
+        idsolicitudenvio: senvioSeleccionado[0].idsolicitudenvio,
+    });
+    const [err, setError] = useState(null);
+
+    const { createEnvio } = useContext(AuthContext);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(inputs);
+        try {
+            if (!inputs.idenvio || !inputs.descripcionpaquete || !inputs.estadoenvio || !inputs.tarifaenvio || !inputs.fechaenviorealizado || !inputs.destinoenvio || !inputs.idrepartidor || !inputs.idsolicitudenvio) {
+                setError("Por favor llene los campos necesarios");
+                return;
+            }
+            else {
+                console.log(inputs);
+                // const res = await createEnvio(inputs);
+                // alert("Envio habilitado con éxito");
+                // window.location.reload();
+            }
+        } catch (err) {
+            setError(err.response.data);
+        }
+
+    }
     return (
         <div className="content-flex">
             <Sidebar />
@@ -31,7 +66,8 @@ const SolEnvInfo2 = () => {
                             <button>Calcular tarifa</button>
                             <p>$ {senvioSeleccionado[0].tarifasolicitud}</p>
                         </div>
-                        <button>Aceptar</button>
+                        {err && <p className="register__bg__error"> {err}</p>}
+                        <button onClick={handleSubmit}>Aceptar</button>
                         <Link to="/Home"><button>Rechazar</button></Link>
                     </div>
                 </div>
