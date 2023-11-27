050d25866566c06d9aa27834888bf6a8f35dd940
Kelly Solano <kelly.solano.1403@gmail.com>
2023-11-27 10:32:39 -0500

    Traza de operaciones con usuario hecha

--- api/controllers/usuario.js
+++ api/controllers/usuario.js
@@ -2,7 +2,7 @@ import { db } from '../db.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { usuario_log } from './auth.js';
-
+import { registrarOperacion } from './auditoria.js';
 
 
 export const updateuser = (req, res) => {
@@ -29,22 +29,34 @@ export const updateuser = (req, res) => {
 
 
         db.query(query_cliente, values, (err, data) => {
-            if (err) return res.json(err);
+            if (err) {
+                registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Cliente", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+                return res.json(err);
+            }
             console.log(data[0]);
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Cliente", req.body.idusuario, `Actualización de cliente con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
             return res.status(200).json("Cliente actualizado con exito");
         });
     } else if (tipo_usuario === "administrador") {
 
         db.query(query_administrador, values, (err, data) => {
-            if (err) return res.json(err);
+            if (err) {
+                registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Administrador", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+                return res.json(err);
+            }
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Administrador", req.body.idusuario, `Actualización de administrador con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
             return res.status(200).json("Administrador actualizado con exito");
         });
 
     } else {
 
         db.query(query_repartidor, values, (err, data) => {
-            if (err) return res.json(err);
+            if (err) {
+                registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Repartidor", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+                return res.json(err);
+            }
             console.log(data[0]);
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Repartidor", req.body.idusuario, `Actualización de repartidor con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
             return res.status(200).json("Repartidor actualizado con exito");
         });
 
@@ -58,8 +70,12 @@ export const getuser = (req, res) => {
     const q = "select * from (select idCliente as idusuario, nombrecliente as nombreusuario, correocliente as correousuario, direccioncliente as direccionusuario, telefonocliente as telefonousuario, contrasenacliente as contrasenausuario, identificadorpregcliente as identificadorpregusuario, respuestapregcliente as respuestapregusuario, tipousuario from cliente union select idadministrador as idusuario, nombreadministrador as nombreusuario, correoadministrador as correousuario, direccionadministrador as direccionusuario, telefonoadministrador as telefonousuario, contrasenaadministrador as contrasenausuario, identificadorpregadmin as identificadorpregusuario, respuestapregadmin as respuestapregusuario, tipousuario from administrador union select idrepartidor as idusuario, nombrerepartidor as nombreusuario, correorepartidor as correousuario, direccionrepartidor as direccionusuario, telefonorepartidor as telefonousuario, contrasenarepartidor as contrasenausuario, identificadorpregrepar as identificadorpregusuario, respuestapregrepar as respuestapregusuario, tipousuario from repartidor) as usuarios where idusuario = ?";
 
     db.query(q, [req.body.idusuario], (err, data2) => {
-        if (err) return res.json(err);
+        if (err) {
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "GET", "Usuario", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+            return res.json(err);
+        }
         console.log(data2[0]);
+        registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "GET", "Usuario", req.body.idusuario, `Consulta de usuario con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
         return res.status(200).json(data2[0]);
     });
 }
@@ -77,8 +93,6 @@ export const updatepass = (req, res) => {
     const query_administrador = "UPDATE administrador SET contrasenaadministrador = ? WHERE idadministrador = ?";
     const query_repartidor = "UPDATE repartidor SET contrasenarepartidor = ? WHERE idrepartidor = ?";
 
-
-
     const hash_nueva = bcrypt.hashSync(nueva_contrasena, salt);
     const hash_antigua = bcrypt.hashSync(antigua_contrasena, salt);
 
@@ -86,25 +100,38 @@ export const updatepass = (req, res) => {
         console.log(nueva_contrasena);
         if (tipo_usuario === "cliente") {
             db.query(query_cliente, [hash_nueva, req.body.idusuario], (err, data) => {
-                if (err) return res.json(err);
+                if (err) {
+                    registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Cliente", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+                    return res.json(err);
+                }
                 console.log(data[0]);
+                registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Cliente", req.body.idusuario, `Actualización de contraseña de cliente con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
                 return res.status(200).json("Contraseña actualizada con exito");
             });
         } else if (tipo_usuario === "administrador") {
             db.query(query_administrador, [hash_nueva, req.body.idusuario], (err, data) => {
-                if (err) return res.json(err);
+                if (err) {
+                    registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Administrador", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+                    return res.json(err);
+                }
                 console.log(data[0]);
+                registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Administrador", req.body.idusuario, `Actualización de contraseña de administrador con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
                 return res.status(200).json("Contraseña actualizada con exito");
             });
     
         } else {
             db.query(query_repartidor, [hash_nueva, req.body.idusuario], (err, data) => {
-                if (err) return res.json(err);
+                if (err) {
+                    registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Repartidor", req.body.idusuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+                    return res.json(err);
+                }
                 console.log(data[0]);
+                registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", "Repartidor", req.body.idusuario, `Actualización de contraseña de repartidor con identificador ${req.body.idusuario} exitosa`, "Exitoso", new Date(), res);
                 return res.status(200).json("Contraseña actualizada con exito");
             });
         }
     }else {
+        registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "UPDATE", usuario_log.tipousuario, req.body.idusuario, `Cambio de contraseña fallido porque la nueva contraseña no puede ser igual a la anterior`, "Fallido", new Date(), res);
         return res.status(400).json("La nueva contraseña no puede ser igual a la anterior");
     }
 
@@ -113,14 +140,18 @@ export const updatepass = (req, res) => {
 export const checkpass = (req, res) => {
     const contrasena_verificar = req.body.contrasenausuario;
     const id_usuario = req.body.idusuario;
-
     const q = "select * from (select idCliente as idusuario, nombrecliente as nombreusuario, correocliente as correousuario, direccioncliente as direccionusuario, telefonocliente as telefonousuario, contrasenacliente as contrasenausuario, identificadorpregcliente as identificadorpregusuario, respuestapregcliente as respuestapregusuario, tipousuario from cliente union select idadministrador as idusuario, nombreadministrador as nombreusuario, correoadministrador as correousuario, direccionadministrador as direccionusuario, telefonoadministrador as telefonousuario, contrasenaadministrador as contrasenausuario, identificadorpregadmin as identificadorpregusuario, respuestapregadmin as respuestapregusuario, tipousuario from administrador union select idrepartidor as idusuario, nombrerepartidor as nombreusuario, correorepartidor as correousuario, direccionrepartidor as direccionusuario, telefonorepartidor as telefonousuario, contrasenarepartidor as contrasenausuario, identificadorpregrepar as identificadorpregusuario, respuestapregrepar as respuestapregusuario, tipousuario from repartidor) as usuarios where idusuario = ? and contrasenausuario = ?";
 
     db.query(q, [id_usuario, contrasena_verificar], (err, data) => {
-        if (err) return res.json(err);
+        if (err) {
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "GET", usuario_log.tipousuario, id_usuario, "Error en conexión con la base de datos", "Fallido", new Date(), res);
+            return res.json(err);
+        }
         if(data.length === 0){
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "GET", usuario_log.tipousuario, id_usuario, `Verificación de contraseña de ${usuario_log.nombreusuario} con identificador ${id_usuario} fallida`, "Fallido", new Date(), res);
             return false;
         }else{
+            registrarOperacion(usuario_log.tipousuario, usuario_log.idusuario, usuario_log.nombreusuario, "GET", usuario_log.tipousuario, id_usuario, `Verificación de contraseña de ${usuario_log.nombreusuario} con identificador ${id_usuario} exitosa`, "Exitoso", new Date(), res);
             return true;
         }
     });
